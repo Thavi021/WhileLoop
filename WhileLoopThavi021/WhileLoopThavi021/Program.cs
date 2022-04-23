e0282336613fc64bd7aabf216c8670790efd2d12
Thaveshan Naidoo <96347605+Thavi021@users.noreply.github.com>
2022-04-23 19:36:57 +0200

    Add files via upload

---
+++ WhileLoopThavi021/WhileLoopThavi021/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WhileLoopThavi021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            string s;
+            while(i < 6)
+            {
+                s = "Output\n";
+                Console.WriteLine(i + ". "+s);
+                i++;
+            }
+            
+        }
+    }
+}
